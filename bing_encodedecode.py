5e569414d5ba8fffd53f3165ebf15d8ba910afd0
Przemyslaw "Blasto" Wentrys <pwentrys@gmail.com>
2017-06-30 08:41:32 -0400

    Logging cleanup and reclassing.

--- bing_encodedecode.py
+++ bing_encodedecode.py
@@ -2,6 +2,7 @@ import sys
 import pathlib
 import time
 import os
+from typing import Tuple
 
 import requests
 from urllib.parse import quote, urlparse
@@ -105,7 +106,7 @@ class TimingsOffset:
         self.day_end = self._update_day_end()
         self.url = self._update_url()
 
-timings = TimingsOffset()
+timings_offset = TimingsOffset()
 
 
 class URLFormat:
@@ -173,7 +174,7 @@ class OutputFormat:
 
     @staticmethod
     def description(string: str) -> str:
-        string = string.replace(timings.bdy, '')
+        string = string.replace(timings_offset.bdy, '')
         string = OutputFormat.entry(string)
         return string
 
@@ -183,13 +184,139 @@ class OutputFormat:
 
 
 class Timings:
-    START = datetime.now()
-    END_EST_SECS = 0
-    END_EST = datetime.now()
-    END_REAL = datetime.now()
+    SLEEP = Statics.SLEEP_TIME
+    ESTIMATE_MULTI = 1.15
+
+    def __init__(self):
+        self._start = 0.0
+        self._stop = 0.0
+        self._stop_estimate = 0.0
+        self._duration_actual = 0.0
+        self._duration_estimate = 0.0
+        self._duration_error = 0.0
+        self._op_its = 0  # operation iterations
+        self._counter = 0  # count which op we're at.
+
+    def start(self):
+        """
+        Start timer.
+        :return:
+        """
+        self._counter, self._start, self._stop_estimate = Timings._calculate_stop_estimate(time.time(), self._duration_estimate)
+
+    @staticmethod
+    def _calculate_stop_estimate(start: float, duration: float) -> Tuple[int, float, float]:
+        """
+        Calculate stop estimates.
+        :param start:
+        :param duration:
+        :return:
+        """
+        return 0, start, start + duration
+
+    def start_logged(self):
+        """
+        Run start and return log print.
+        :return:
+        """
+        self.start()
+        print('\n'.join([
+            f'Start Time: {self._start}',
+            f'Est Run: {self._duration_estimate}',
+            f'Est Stop: {self._stop_estimate}',
+            f'Op Its: {self._op_its}'
+        ]))
+
+    def stop(self):
+        """
+        Stop timer.
+        :return:
+        """
+        self._stop, self._duration_actual, self._duration_error = Timings._calculate_duration(self._start, time.time(), self._duration_estimate)
+
+    def stop_logged(self):
+        """
+        Run stop and return log print.
+        :return:
+        """
+        self.stop()
+        print('\n'.join([
+            f' ---- Times  ---- ',
+            f'Started: {self._start}',
+            f'Finished: {self._stop}',
+            f'Estimated: {self._stop_estimate}',
+            f' ----- Run  ----- ',
+            f'Actual: {self._duration_actual}',
+            f'Estimated: {self._duration_estimate}',
+            f'Error: {self._duration_error}',
+            f' ---------------- '
+        ]))
+
+    @staticmethod
+    def _calculate_duration_and_diff(estimate: float, actual: float) -> Tuple[float, float]:
+        """
+        Calculate duration and errors.
+        :param estimate:
+        :param actual:
+        :return:
+        """
+        return actual, (estimate - actual) if estimate > actual else (actual - estimate)
+
+    @staticmethod
+    def _calculate_duration(start: float, stop: float, estimate: float) -> Tuple[float, float, float]:
+        """
+        Get duration and errors calculated.
+        :param start:
+        :param stop:
+        :param estimate:
+        :return:
+        """
+        duration, difference = Timings._calculate_duration_and_diff(estimate, stop - start)
+        return stop, duration, difference
+
+    @staticmethod
+    def _estimate_duration(op_its: int, offset_multi: float) -> Tuple[float, float]:
+        """
+        Calculate run time estimate.
+
+        (Operations x Iterations) x (Sleep Time x Offset Multi)
+        :param op_its:
+        :param offset_multi:
+        :return:
+        """
+        return op_its, op_its * offset_multi
+
+    def estimate_duration(self, operations: int, iterations: int):
+        """
+        Get run time estimate.
+        :param operations:
+        :param iterations:
+        :return:
+        """
+        self._op_its, self._duration_estimate = Timings._estimate_duration(operations * iterations, self.SLEEP * self.ESTIMATE_MULTI)
+
+    @staticmethod
+    def _operation_logged(counter: int, total: int, start: float) -> int:
+        """
+        Print log and return incremented counter.
+        :param counter:
+        :param total:
+        :param start:
+        :return:
+        """
+        print(f'{counter} / {total}     -     Elapsed: {time.time() - start}')
+        return counter
+
+    def operation_logged(self):
+        """
+        Increment counter and log progress.
+        :return:
+        """
+        self._counter = Timings._operation_logged(self._counter + 1, self._op_its, self._start)
 
 
-Timings.START = datetime.now()
+timer = Timings()
+# Timings.START = time.time()  # datetime.now()
 tsv_path = pathlib.Path(os.path.join(f'{Statics.DATA_DIR}', 'TSLA.tsv'))
 tsv_path_previous = pathlib.Path(os.path.join(f'{Statics.DATA_DIR}', 'TSLA_PREV.tsv'))
 if not tsv_path.is_file():
@@ -210,27 +337,23 @@ def do_update_file_write(additions: list):
 
 
 def run(offset_start: int, offset_end: int, pages: int):
-    total_runs = ((offset_end - offset_start) * pages)
-    print(f'Start Time: {Timings.START}')
-    Timings.END_EST_SECS = total_runs * (Statics.SLEEP_TIME * 1.15)
-    Timings.END_EST = Timings.START + timedelta(seconds=Timings.END_EST_SECS)
-    print(f'Finish Est: {Timings.END_EST}   -   ({Timings.END_EST - Timings.START})')
+    timer.estimate_duration(offset_end - offset_start, pages)
+    # total_runs = ((offset_end - offset_start) * pages)
+    timer.start_logged()
+    # print(f'Start Time: {Timings.START}')
+    # Timings.END_EST_SECS = total_runs * (Statics.SLEEP_TIME * 1.15)
+    # Timings.END_EST = Timings.START + timedelta(seconds=Timings.END_EST_SECS)
+    # print(f'Finish Est: {Timings.END_EST}   -   ({Timings.END_EST - Timings.START})')
     current_runs = 0
     final_results = []
     for i in range(offset_start, offset_end):
-        if len(final_results) > 1000:
-            do_update_file_write(final_results)
-            final_results.clear()
-            if len(final_results) > 0:
-                final_results = []
-
-        timings.int = i
-        timings.update()
+        timings_offset.int = i
+        timings_offset.update()
         for j in range(0, pages):
             current_runs += 1
-            print(f'Starting {current_runs} / {total_runs}   -   ({datetime.now() - Timings.START}).')
+            # print(f'Starting {current_runs} / {total_runs}   -   ({datetime.now() - Timings.START}).')
             time.sleep(Statics.SLEEP_TIME)
-            res = requests.get(timings.url_with_first_offset(j))
+            res = requests.get(timings_offset.url_with_first_offset(j))
             txt = res.text
 
             requests_html = pathlib.Path(os.path.join(Statics.WORK_DIR, 'requests_bing.html'))
@@ -254,35 +377,31 @@ def run(offset_start: int, offset_end: int, pages: int):
                 desc = soupr_d[i]
 
                 _desc = f'{desc.text}'.strip()
-                if _desc.startswith(timings.bdy):
+                if _desc.startswith(timings_offset.bdy):
                     _continue = True
                 else:
                     soup_dt = BeautifulSoup(str(title.parent.parent), Statics.PARSER)
                     dt = soup_dt.find_all(TagExtract.a_link_news_dt)
                     if len(dt) > 0:
                         _dt = OutputFormat.entry(dt[0].text)
-                        if _dt == timings.days_ago:
+                        if _dt == timings_offset.days_ago:
                             _continue = True
 
                 if _continue:
                     _desc = OutputFormat.description(_desc)
                     _text = OutputFormat.title(title.text)
                     _href = OutputFormat.link(title.get('href'))
-                    final_results.append(f'{timings.ymd}|{_href}|{_text}|{_desc}')
+                    final_results.append(f'{timings_offset.ymd}|{_href}|{_text}|{_desc}')
+
+        if len(final_results) > 1000:
+            do_update_file_write(final_results)
+            final_results.clear()
+            if len(final_results) > 0:
+                final_results = []
 
     do_update_file_write(final_results)
+    timer.stop_logged()
 
 if __name__ == '__main__':
-    run(0, 3000, 3)
-    # cur_text = tsv_path.read_text(encoding=Statics.UTF8)
-    # if text != cur_text:
-    # tsv_path.write_text(text, encoding=Statics.UTF8)
-    Timings.END_REAL = datetime.now()
-    print(f'Finished At: {Timings.END_REAL}')
-    print(f'Original Est: {Timings.END_EST}')
-    if Timings.END_REAL > Timings.END_EST:
-        print(f'UNDERESTIMATED BY {Timings.END_REAL - Timings.END_EST}')
-    else:
-        print(f'OVERESTIMATED BY {Timings.END_EST - Timings.END_REAL}')
-    print(f'Total Run: {Timings.END_REAL - Timings.START}')
+    run(0, 3, 3)
 
